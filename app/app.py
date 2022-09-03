c1becf926cf2fc65511444cafd91f6d5894ef02e
Pauline Hidalgo <hidalgopauline@gmail.com>
2022-09-03 11:57:33 -0400

    reformat code, generate requirements

--- app/app.py
+++ app/app.py
@@ -31,7 +31,7 @@ st.markdown("# Plant tracker :seedling:")
 
 st.date_input("Current date")
 
-col1, col2, col3 = st.columns(3) # plant, days since water
+col1, col2, col3 = st.columns(3)  # plant, days since water
 selected_plants = []
 
 col1.subheader("Plant")
@@ -41,20 +41,18 @@ col3.subheader("Actions")
 col3.button("Water")
 col3.button("Fertilize")
 
-for item in items: 
-    name = item['name']
-    last_watered = datetime.strptime(item['dates_watered'][-1], '%m/%d/%Y')
+for item in items:
+    name = item["name"]
+    last_watered = datetime.strptime(item["dates_watered"][-1], "%m/%d/%Y")
     days_since_water = (datetime.now() - last_watered).days
 
-    with col1: 
+    with col1:
         plant_select = st.checkbox(name)
-        if plant_select: 
+        if plant_select:
             selected_plants.append(name)
-    
-    with col2: 
+
+    with col2:
         st.write(f"{days_since_water} days ago - {last_watered.strftime('%b %d, %Y')}")
-    
+
 
 st.write(selected_plants)
-
-
