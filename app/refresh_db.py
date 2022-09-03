45939a968af7478e0204185989eb5a728fea852e
Pauline Hidalgo <hidalgopauline@gmail.com>
2022-09-03 18:58:21 -0400

    rename file

--- app/refresh_db.py
+++ app/refresh_db.py
@@ -3,6 +3,7 @@ import pymongo
 import pandas as pd
 
 url = f'mongodb+srv://{st.secrets["mongo"]["username"]}:{st.secrets["mongo"]["password"]}@plantbase.zb3enmb.mongodb.net/?retryWrites=true&w=majority'
+data_path = "C:\\Users\\hidal\\Downloads\\plant watering data - data.csv"
 
 # Initialize connection.
 # Uses st.experimental_singleton to only run once.
@@ -15,11 +16,12 @@ client = init_connection()
 db = client.plantbase
 coll = db.myplants
 
-data = pd.read_csv("/Users/hidalpe1/Downloads/plant watering data - data.csv")
+data = pd.read_csv(data_path)
 docs = []
 for column in data.columns:
     docs.append({"name": column, "dates_watered": data[column].dropna().tolist(), "dates_fertilized": []})
 
+db.myplants.delete_many({})
 result = coll.insert_many(docs)
 
 client.close()
